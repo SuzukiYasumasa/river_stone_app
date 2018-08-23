518e5cfbd821ef70d69c9b638f51cf87779bec41
vagrant <vagrant@localhost.localdomain>
2018-08-23 01:13:04 +0000

    Modfy point views

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,6 +1,6 @@
 class Point < ApplicationRecord
   belongs_to :construction
-  validates :name, :lng, :lat, :construction_id, :release, presence: true
+  validates :name, :lng, :lat, :construction_id, presence: true
   validates :name, length: { maximum: 20 }
   validates :lng, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
   validates :lat, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
