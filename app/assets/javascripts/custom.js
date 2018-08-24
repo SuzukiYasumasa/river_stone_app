5c82126424accccb854923cc45b0c02b3d5fc4b3
vagrant <vagrant@localhost.localdomain>
2018-08-24 07:04:05 +0000

    complete sign_in_state view

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,6 +2,11 @@ $(document).on('turbolinks:load', function(){
   if(document.URL.match("/users/sign_in")) {
     $('#log_in_icon').remove();
   }
+
+  $('div[id^=points-container-]').each(function(index, element) {
+    $(this).children(".row").filter(':even').css('background', '#e7e7eb');
+  });
+
 });
 $(document).ready(function(){
 });
