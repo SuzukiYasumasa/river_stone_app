6f365249a7c6989d85d525aa9bac4e37a7f7de59
vagrant <vagrant@localhost.localdomain>
2018-09-07 07:28:22 +0000

    Complete hamburger button

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -10,10 +10,29 @@ $(document).on('turbolinks:load', function(){
   $('.visitor-construction:first').css('margin-top', '0');
 
   $('#mapview').css("height", $(window).height() - 50 + "px");
+
+  $('.menu-trigger').click(function() {
+    $(this).toggleClass('active');
+    if($(this).hasClass('active')) {
+      $('#background-shadow').show();
+      $('#hamburger-menu').addClass('active');
+    } else {
+      $('#background-shadow').hide();
+      $('#hamburger-menu').removeClass('active');
+    }
+  });
 });
 
 $(window).on('resize', function(){
+  //googlemap用要素の高さをウィンドウサイズに合わせて変更
   $('#mapview').css("height", $(window).height() - 50 + "px");
+
+  //ウィンドウ横幅が広がった時(bootstrap4でいうところのsmになった時)ハンバーガ関連要素を見えなくする
+  if($(window).innerWidth() >= 576) {
+    $('.menu-trigger').removeClass('active');
+    $('#background-shadow').hide();
+    $('#hamburger-menu').removeClass('active');
+  }
 });
 
 $(document).ready(function(){
